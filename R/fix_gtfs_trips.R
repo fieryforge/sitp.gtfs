ed56883a0762064b3c7060bdd704a875576c7b97
ferragus <fieryforgermen@gmail.com>
2025-11-18 23:57:31 -0500

    Push to github

--- R/fix_gtfs_trips.R
+++ R/fix_gtfs_trips.R
@@ -7,5 +7,47 @@
 #'
 #' @param trips A data.table returned by `get_gtfs_stops()`.
 fix_gtfs_trips <- function(trips) {
+  data.table::setkeyv(trips, c("route_id", "stop_sequence"))
+  sf_trips <- lapply(X = unique(trips$route_id)[719:722],
+                     FUN = function(r) {
+                       st_r <- sf::st_as_sf(trips[r, on = "route_id"],
+                                            coords = c("stop_lon", "stop_lat"),
+                                            crs = 4326)
+                       st_r_clean <- remove_trouble_point(st_r)
+                     })
 
+}
+
+remove_trouble_point <- function(st_r) {
+  # Calculate consecutive distances using Method 3
+  distances <- numeric(nrow(st_r) - 1)
+
+  for(i in 1:(nrow(st_r) - 1)) {
+    distances[i] <- sf::st_distance(st_r[i, ], st_r[i + 1, ])
+  }
+
+  # Calculate median distance
+  median_distance <- median(distances)
+
+  # Check conditions for removing first or last point
+  first_distance <- distances[1]  # Distance between pt1 and pt2
+  last_distance <- distances[length(distances)]  #Distance between ptn and ptn-1
+
+  # Apply your removal logic
+  if (first_distance > (2 * median_distance)) {
+    # Remove point 1
+    st_r_clean <- st_r[-1, ]
+    message("Removed first point: distance = ",
+            first_distance, " > 2 * median = ", 2 * median_distance)
+  } else if (last_distance > (2 * median_distance)) {
+    # Remove point 20
+    st_r_clean <- st_r[-nrow(st_r), ]
+    message("Removed last point: distance = ",
+            last_distance, " > 2 * median = ", 2 * median_distance)
+  } else {
+    # Keep both points
+    st_r_clean <- st_r
+    message("No points removed - both endpoints within acceptable distance")
+  }
+  return(st_r_clean)
 }
