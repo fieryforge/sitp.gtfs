7a12853c222c311bc091714ffa6e8b120dd9807d
ferragus <fieryforgermen@gmail.com>
2025-11-23 00:07:19 -0500

    Changed points removal method
    Before points where removed by median difference, changed to remove
    by distance between first and last points.

--- R/fix_gtfs_trips.R
+++ R/fix_gtfs_trips.R
@@ -6,47 +6,43 @@
 #' loops.
 #'
 #' @param trips A data.table returned by `get_gtfs_stops()`.
+#' @return A list of sfc objects for 'each route_id'.
 fix_gtfs_trips <- function(trips) {
   data.table::setkeyv(trips, c("route_id", "stop_sequence"))
-  sf_trips <- lapply(X = unique(trips$route_id)[719:722],
+  sf_trips <- lapply(X = unique(trips$route_id),
                      FUN = function(r) {
-                       st_r <- sf::st_as_sf(trips[r, on = "route_id"],
+                       sfc_route <- sf::st_as_sf(trips[r, on = "route_id"],
                                             coords = c("stop_lon", "stop_lat"),
                                             crs = 4326)
-                       st_r_clean <- remove_trouble_point(st_r)
+                       sfc_route_clean <- remove_head_tail(sfc_route)
                      })
 
 }
 
-remove_trouble_point <- function(st_r) {
-  # Calculate consecutive distances
-  distances <- numeric(nrow(st_r) - 1)
+#' Remove the first or last point in route on condition
+#'
+#' @param sfc_route An sf sfc route object.
+#' @return The input object with points removed if condition met.
+remove_head_tail <- function(sfc_route) {
+  head_distance <- st_distance(sfc_route[1, ], sfc_route[2, ])
+  tail_distance <- st_distance(sfc_route[nrow(sfc_route), ], sfc_route[nrow(sfc_route) - 1, ])
 
-  for(i in 1:(nrow(st_r) - 1)) {
-    distances[i] <- sf::st_distance(st_r[i, ], st_r[i + 1, ])
-  }
+  distance_difference <- abs(head_distance - tail_distance)
+  distance_threshold <- units::set_units(500, "m")
 
-  # Calculate median distance
-  median_distance <- median(distances)
-
-  # Check conditions for removing first or last point
-  first_distance <- distances[1]  # Distance between pt1 and pt2
-  last_distance <- distances[length(distances)]  #Distance between ptn and ptn-1
-
-  if (first_distance > (2 * median_distance)) {
-    # Remove point 1
-    st_r_clean <- st_r[-1, ]
-    message("Removed first point: distance = ",
-            first_distance, " > 2 * median = ", 2 * median_distance)
-  } else if (last_distance > (2 * median_distance)) {
-    # Remove point N
-    st_r_clean <- st_r[-nrow(st_r), ]
-    message("Removed last point: distance = ",
-            last_distance, " > 2 * median = ", 2 * median_distance)
+  if (distance_difference > distance_threshold) {
+    if (head_distance > tail_distance) {
+      sfc_route <- sfc_route[-1, ]
+      sfc_route$stop_sequence <- seq(nrow(sfc_route))
+      message("Removed first point on route: ", sfc_route$route_id[1], " distance diffence = ", distance_difference)
+    } else {
+      sfc_route <- sfc_route[-nrow(sfc_route), ]
+      message("Removed last point on route: ", sfc_route$route_id[1], " distance diffence = ", distance_difference)
+    }
   } else {
-    # Keep both points
-    st_r_clean <- st_r
-    message("No points removed - both endpoints within acceptable distance")
+    message("No points removed on route: ", sfc_route$route_id[1])
+    sfc_route
   }
-  return(st_r_clean)
+
+  return(sfc_route)
 }
